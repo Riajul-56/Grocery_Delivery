300a60fd834538add4e2cc505b9c8e1aa7e7ffdf
Riajul-56 <riajulhasansiam@gmail.com>
2026-01-30 13:31:17 +0600

    Refactor database connection logic in db.ts and remove dotenv import; update error handling

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,78 +1,33 @@
-// import mongoose from "mongoose";
-
-// const mongodbUrl = process.env.MONGODB_URL;
-
-// if (!mongodbUrl) {
-//   throw new Error("db error");
-// }
-
-// let cached = global.mongoose;
-// if (!cached) {
-//   cached = global.mongoose = { conn: null, promise: null };
-// }
-
-// const connectDb = async () => {
-//   if (cached.conn) {
-//     return cached.conn;
-//   }
-
-//   if (!cached.promise) {
-//     cached.promise = mongoose
-//       .connect(mongodbUrl)
-//       .then((conn) => conn.connection);
-//   }
-//   try {
-//     const conn = await cached.promise;
-//     return conn;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export default connectDb;
-
-
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-// Load environment variables if they are not already loaded
-dotenv.config();
 
 const mongodbUrl = process.env.MONGODB_URL;
 
 if (!mongodbUrl) {
-  throw new Error("MONGODB_URL is not defined in environment variables");
+  throw new Error("db error");
 }
 
 let cached = global.mongoose;
-
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
 
-const connectdb = async () => {
+const connectDb = async () => {
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(mongodbUrl, opts).then((mongoose) => {
-      return mongoose.connection;
-    });
+    cached.promise = mongoose
+      .connect(mongodbUrl)
+      .then((conn) => conn.connection);
   }
-
   try {
-    cached.conn = await cached.promise;
-    return cached.conn;
+    const conn = await cached.promise;
+    return conn;
   } catch (error) {
-    cached.promise = null;
-    throw error;
+    console.log(error);
   }
 };
 
-export default connectdb;
+export default connectDb;
 
