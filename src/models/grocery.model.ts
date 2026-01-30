a6aeb0830525d259d64f8e37487002b2507cd449
Riajul-56 <riajulhasansiam@gmail.com>
2026-01-30 17:10:40 +0600

    Remove dotenv dependency and user model; add grocery model with schema definition

---
+++ src/models/grocery.model.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+interface IGrocery {
+  _id?: mongoose.Types.ObjectId;
+  name: string;
+  category: string;
+  price: string;
+  unit: string;
+  image: string;
+  createAt?: Date;
+  updatedAt?: Date;
+}
+
+const grocerySchema = new mongoose.Schema<IGrocery>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        "Fruits & Vegetables",
+        "Dairy & Eggs",
+        "Rice, Atta & Grains",
+        "Snacks & Biscuits",
+        "Spices & Masalas",
+        "Beverages & Drinks",
+        "Personal Care",
+        "Household Essentials",
+        "Instant & Packaged Food",
+        "Baby & Pet Care",
+      ],
+      required: true,
+    },
+    price: {
+      tyoe: String,
+      required: true,
+    },
+    unit: {
+      tyoe: String,
+      required: true,
+    },
+    image: {
+      tyoe: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Grocery =
+  mongoose.models.Grocery || mongoose.model("Grocery", grocerySchema);
+
+export default Grocery;
