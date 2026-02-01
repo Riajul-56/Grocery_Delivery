6a6049f17c8f3f8e91f60f9f971f28bd97d67920
Riajul-56 <riajulhasansiam@gmail.com>
2026-02-02 00:58:25 +0600

    Enhance Navbar with role-based icons and links for user and admin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 "use client";
 import {
+  Boxes,
+  ClipboardCheckIcon,
   LogOut,
   Package,
+  PlusCircle,
   Search,
   ShoppingCartIcon,
   User,
@@ -45,6 +48,8 @@ const Navbar = ({ user }: { user: IUser }) => {
 
   return (
     <div className="w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-green-500 to-green-700 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-20 px-4 md:px-8 z-50">
+      {/* ============== Snapcart Icon for Navbar ========================== */}
+
       <Link
         href={"/"}
         className="text-white font-extrabold text-2xl sm:text-3xl tracking-wide hover:scale-105 transition-transform "
@@ -52,32 +57,75 @@ const Navbar = ({ user }: { user: IUser }) => {
         Snapcart
       </Link>
 
-      <form className="hidden md:flex items-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg shadow-md">
-        <Search className="text-gray-500 w-5 h-5 mr-2" />
-        <input
-          type="text"
-          placeholder="Search groceries..."
-          className="w-full outline-none text-gray-700 placeholder-gray-400"
-        />
-      </form>
+      {/* ============== search bar for user ========================== */}
+
+      {user.role == "user" && (
+        <form className="hidden md:flex items-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg shadow-md">
+          <Search className="text-gray-500 w-5 h-5 mr-2" />
+          <input
+            type="text"
+            placeholder="Search groceries..."
+            className="w-full outline-none text-gray-700 placeholder-gray-400"
+          />
+        </form>
+      )}
+
+      {/* ============== Search icon for small device and user ========================== */}
 
       <div className="flex items-center gap-3 md:gap-4 relative">
-        <div
-          className="bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition md:hidden"
-          onClick={() => setSearchBarOpen((prev) => !prev)}
-        >
-          <Search className="text-green-600 w-6 h-6" />
-        </div>
+        {user.role == "user" && (
+          <>
+            <div
+              className=" bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition md:hidden"
+              onClick={() => setSearchBarOpen((prev) => !prev)}
+            >
+              <Search className="text-green-600 w-6 h-6" />
+            </div>
 
-        <Link
-          href={""}
-          className="relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition"
-        >
-          <ShoppingCartIcon className="text-green-600 w-6 h-6" />
-          <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow">
-            0
-          </span>
-        </Link>
+            {/* ============== Cart Icon for user  ========================== */}
+
+            <Link
+              href={""}
+              className="relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition"
+            >
+              <ShoppingCartIcon className="text-green-600 w-6 h-6" />
+              <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full font-semibold shadow">
+                0
+              </span>
+            </Link>
+          </>
+        )}
+
+        {/* ============== Cart Icon for admin Start ========================== */}
+
+        {user.role == "admin" && (
+          <>
+            <div className="hidden md:flex items-center gap-4">
+              <Link
+                href={""}
+                className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all hover:scale-105"
+              >
+                <PlusCircle className="w-5 h-5" />
+                Add Grocery
+              </Link>
+              <Link
+                href={""}
+                className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all hover:scale-105"
+              >
+                <Boxes className="w-5 h-5" />
+                View Grocery
+              </Link>
+              <Link
+                href={""}
+                className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all hover:scale-105 "
+              >
+                <ClipboardCheckIcon className="w-5 h-5" />
+                Manage Orders
+              </Link>
+            </div>
+          </>
+        )}
+        {/* ============== Cart Icon for admin End  ========================== */}
 
         <div className="relative" ref={profileDropDown}>
           <div
