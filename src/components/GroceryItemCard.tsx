d8de5a236bc82e47e0378606e82090ae11b78ad8
Riajul-56 <riajulhasansiam@gmail.com>
2026-02-07 12:34:30 +0600

    gorcery item card design completed

--- src/components/GroceryItemCard.tsx
+++ src/components/GroceryItemCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { PlayCircle, PlusCircle, ShoppingCart } from "lucide-react";
 import mongoose from "mongoose";
 import { motion } from "motion/react";
 import Image from "next/image";
@@ -32,7 +33,7 @@ const GroceryItemCard = ({ item }: { item: IGrocery }) => {
       }}
       viewport={{
         once: false,
-        amount: 0.5,
+        amount: 0.3,
       }}
     >
       <div className="relative w-full aspect-4/3 bg-gray-50 overflow-hidden group">
@@ -43,6 +44,33 @@ const GroceryItemCard = ({ item }: { item: IGrocery }) => {
           sizes="(max-width:768px)100vw,25vw"
           className="object-contain p-4 transition-transform duration-500 group-hover:scale-150"
         />
+        <div className="absolute inset-0 bg-linear-to-t from-black/10 to-transparent opacity-0 group-hover:opacity-100 transition-all duration-300 " />
+      </div>
+
+      <div className="p-4 flex flex-col flex-1">
+        <p className="text-xs text-gray-500 font-medium mb-1">
+          {item.category}
+        </p>
+        <h3>{item.name}</h3>
+
+        <div className="flex items-center justify-between mt-2">
+          <span className="text-xs font-medium text-gray-600 bg-gray-100 px-2 py-1 rounded-full ">
+            {item.unit}
+          </span>
+          <span className="text-green-700 font-bold text-lg">
+            ৳{item.price}
+          </span>
+        </div>
+
+        <motion.button
+          className="cursor-pointer mt-4 flex items-center justify-center gap-2 bg-green-600 hover:bg-green-700 text-white rounded-full py-2 text-sm font-medium transition-all "
+          whileTap={{
+            scale: 0.96,
+          }}
+        >
+          <ShoppingCart />
+          Add to Cart
+        </motion.button>
       </div>
     </motion.div>
   );
