a00805c59cdffdfb4189896de53f45f0f5e0ccd2
Riajul-56 <riajulhasansiam@gmail.com>
2026-02-24 23:09:58 +0600

    Implement Stripe checkout session creation for order payments

--- src/app/api/user/payment/route.ts
+++ src/app/api/user/payment/route.ts
@@ -31,5 +31,24 @@ export async function POST(req: NextRequest) {
       address,
       totalAmount,
     });
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      success_url: `${process.env.NEXT_BASE_URL}/user/order_success`,
+      cancel_url: `${process.env.NEXT_BASE_URL}/user/order_cancel`,
+      line_items: [
+        {
+          price_data: {
+            currency: "bdt",
+            product_data: {
+              name: "SnapCart Order Payment",
+            },
+            unit_amount: totalAmount * 100,
+          },
+          quantity: 1,
+        },
+      ],
+    });
   } catch (error) {}
 }
