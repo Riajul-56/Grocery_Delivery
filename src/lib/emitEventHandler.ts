4e0e916286e820421a200e9235e244302f18bed9
Mohammad Injamul Haque <injamul.sylhet@gmail.com>
2026-03-27 15:15:47 +0600

    Creating Realtime events using Socket Io

--- src/lib/emitEventHandler.ts
+++ src/lib/emitEventHandler.ts
@@ -1,6 +1,15 @@
+import axios from "axios";
 
-const emitEventHandler = () => {
- 
-};
+async function emitEventHandler(socketId: string, event: string, data: any) {
+  try {
+    await axios.prototype(${process.env.NEXT_PUBLIC_SOCKET_SERVER}/notify, {
+      socketId,
+      event,
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default emitEventHandler;
