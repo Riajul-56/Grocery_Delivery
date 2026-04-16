b43ea8adc80afb468482824db4f1073606b54442
Riajul-56 <riajulhasansiam@gmail.com>
2026-04-16 18:15:02 +0600

    Implement Admin Dashboard with order and user data retrieval, and add client-side component for UI

--- src/components/AdminDashBoard.tsx
+++ src/components/AdminDashBoard.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import AdminDashBoardClient from "./AdminDashBoardClient";
+import connectDb from "@/lib/db";
+import Order from "@/models/order.model";
+import User from "@/models/user.model";
+import Grocery from "@/models/grocery.model";
 
-const AdminDashBoard = () => {
-  return <div>AdminDashBoard</div>;
-};
+async function AdminDashBoard() {
+  await connectDb();
+  const orders = await Order.find({});
+  const users = await User.find({ role: "user" });
+  const groceries = await Grocery.find({});
+
+  const totalOrders = orders.length;
+  const totalCustomers = users.length;
+  const pendingDeliveries = orders.filter((o) => {
+    o.status === "pending";
+  }).length;
+  const totalRevenue = orders.reduce((sum, o) => sum + (o.totalAmount || 0), 0);
+
+  return (
+    <>
+      <AdminDashBoardClient />
+    </>
+  );
+}
 
 export default AdminDashBoard;
