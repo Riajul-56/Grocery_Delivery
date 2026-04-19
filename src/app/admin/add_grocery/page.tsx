b061a86ea35ca7321a0cca6e03553ae3196f2232
Riajul-56 <riajulhasansiam@gmail.com>
2026-04-20 02:21:06 +0600

    fix: add key prop to unit options in AddGrocery component for improved rendering

--- src/app/admin/add_grocery/page.tsx
+++ src/app/admin/add_grocery/page.tsx
@@ -155,8 +155,10 @@ function AddGrocery() {
                 value={unit}
               >
                 <option value="">Select Unit</option>
-                {units.map((cat) => (
-                  <option value={cat}>{cat}</option>
+                {units.map((cat, i) => (
+                  <option key={i} value={cat}>
+                    {cat}
+                  </option>
                 ))}
               </select>
             </div>
